af53e3449af268f8d76a90257d0e8a24f0ea63dc
Kcang-gna <kcang_gna@163.com>
2026-04-13 12:38:51 +0800

    var over

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,11 +1,10 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let x;
-    if x == 10 {
+    let x = 10;
+    if 10 == x {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
